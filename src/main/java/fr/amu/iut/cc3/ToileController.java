a06281ea4ffecc60de6e30a26cf67397731921bb
clement <clement.salomon@etu.univ-amu.fr>
2023-06-02 14:48:40 +0200

    Partie 2 - Dessiner des points après ajout des notes

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -12,17 +12,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+
 
 public class ToileController implements Initializable {
 
@@ -31,6 +34,21 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
+    @FXML
+    TextField TF1;
+    @FXML
+    TextField TF2;
+    @FXML
+    TextField TF3;
+    @FXML
+    TextField TF4;
+    @FXML
+    TextField TF5;
+    @FXML
+    TextField TF6;
+
+    @FXML
+    Pane scene;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -40,10 +58,89 @@ public class ToileController implements Initializable {
         return (int) (rayonCercleExterieur + Math.cos(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
     }
-
     int getYRadarChart(double value, int axe ){
         return (int) (rayonCercleExterieur - Math.sin(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
     }
+
+// Dessiner un point
+    private void Dessiner(int val, int comp) {
+        int x = getXRadarChart(val,comp);
+        int y = getYRadarChart(val,comp);
+
+        Circle circle = new Circle();
+        circle.setCenterX(x);
+        circle.setCenterY(y);
+        circle.setRadius(7);
+
+        scene.getChildren().add(circle);
+
+        System.out.println("Cercle val : "+val+", comp : "+comp);
+    }
+
+
+// Tracer et Vider
+    @FXML
+    private void tracerClicked() {
+        System.out.println("test trace");
+    }
+    @FXML
+    private void viderClicked() {
+        System.out.println("test vide");
+    }
+
+
+
+// Get le contenu des Text Field (TF)
+    @FXML
+    private void get1() {
+        int val = 0;
+        if (TF1.getText().length()>0) {
+            val = Integer. valueOf(TF1.getText());
+        }
+        Dessiner(val,1);
+    }
+    @FXML
+    private void get2() {
+        int val = 0;
+        if (TF2.getText().length()>0) {
+            val = Integer. valueOf(TF2.getText());
+        }
+        Dessiner(val,2);
+    }
+    @FXML
+    private void get3() {
+        int val = 0;
+        if (TF3.getText().length()>0) {
+            val = Integer. valueOf(TF3.getText());
+        }
+        Dessiner(val,3);
+    }
+    @FXML
+    private void get4() {
+        int val = 0;
+        if (TF4.getText().length()>0) {
+            val = Integer. valueOf(TF4.getText());
+        }
+        Dessiner(val,4);
+    }
+    @FXML
+    private void get5() {
+        int val = 0;
+        if (TF5.getText().length()>0) {
+            val = Integer. valueOf(TF5.getText());
+        }
+        Dessiner(val,5);
+    }
+    @FXML
+    private void get6() {
+        int val = 0;
+        if (TF6.getText().length()>0) {
+            val = Integer. valueOf(TF6.getText());
+        }
+        Dessiner(val,6);
+    }
+
+
 
 }
